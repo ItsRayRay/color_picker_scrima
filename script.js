19276ed8b3215d0ad421ed65ae6ac75226ad85ca
ItsRayRay <r.boerema21@gmail.com>
2022-06-05 17:39:27 +0200

    add function button click generates json from api

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-import fetch from "node-fetch";
+
+
+document.querySelector("button").addEventListener("click", function(){
+
+
+    // get value from colorpicker
+    let color = document.querySelector("#favcolor").value;
+
+    // remove hashtag from hex value
+
+    color = color.substring(1);
+
+    // fetch api
+    fetch("https://www.thecolorapi.com/scheme?hex="+color+"&mode="+"monochrome")
+    .then(res => res.json())
+    .then(data => console.log(data) )
 
 
 
-fetch ('https://www.thecolorapi.com/id?hex=0047AB')
-.then(response => response.json())
-.then(data => console.log(data) );
 
+} )
